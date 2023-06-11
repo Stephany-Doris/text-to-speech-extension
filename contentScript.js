67368af74a652e5b903e434a00ca15482582b756
Stephany-Doris <stephany.awino@twiga.com>
2023-06-11 10:53:01 +0300

    fix extension not playing sound, update styles

--- contentScript.js
+++ contentScript.js
@@ -3,7 +3,6 @@ chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
       let articleElement = document.querySelector("article");
       if (articleElement) {
         let text = articleElement.textContent;
-        console.log({text});
         sendResponse({ content: text });
       } else {
         sendResponse({ content: null });
--- popup.js
+++ popup.js
@@ -1,9 +1,27 @@
+let utterance = new SpeechSynthesisUtterance();
+let tabId;
+
 let playButton = document.getElementById("playButton");
 let stopButton = document.getElementById("stopButton");
-let tabId;
+let rateButton = document.getElementById("rate");
 
 playButton.addEventListener("click", playTextToSpeech);
 stopButton.addEventListener("click", stopTextToSpeech);
+rateButton.addEventListener("input", onChangeRate);
+
+document.querySelector("#volume").addEventListener("input", () => {
+  const volume = document.querySelector("#volume").value;
+  let utterance = new SpeechSynthesisUtterance();
+  utterance.volume = volume;
+  document.querySelector("#volume-label").innerHTML = volume;
+});
+
+function onChangeRate() {
+  const rate = document.querySelector("#rate").value;
+  let utterance = new SpeechSynthesisUtterance();
+  utterance.rate = rate;
+  document.querySelector("#rate-label").innerHTML = rate;
+}
 
 function playTextToSpeech() {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
@@ -26,13 +44,28 @@ function stopTextToSpeech() {
 
 function playArticleContent() {
   let articleElement = document.querySelector("article");
+  console.log({articleElement});
   if (articleElement) {
+    let utterance = new SpeechSynthesisUtterance();
     let text = articleElement.textContent;
-    let utterance = new SpeechSynthesisUtterance(text);
+    utterance.text = text;
     speechSynthesis.speak(utterance);
+
+    if(window.speechSynthesis.speaking){
+      window.speechSynthesis.pause();
+    }
+    if(window.speechSynthesis.paused){
+      window.speechSynthesis.resume();
+    }
+    // code to pause if utterance has been started
+    // document.querySelector("#pause").addEventListener("click", () => {
+    //   window.speechSynthesis.pause();
+    // });
+    // window.speechSynthesis.resume();
   }
 }
 
 function stopArticleContent() {
-  speechSynthesis.cancel();
+  console.log("stop");
+  window.speechSynthesis.cancel();
 }
